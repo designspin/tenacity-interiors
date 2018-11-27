31a79af76b69e54ba5b7d7daf6068a203c495cbb
Jason Foster <jason@designspin.co.uk>
2018-11-27 22:38:52 +0000

    Added share buttons component;

--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import Content, { HTMLContent } from '../components/Content';
 import Banner from '../components/BannerLanding';
+import Share from '../components/Share';
 import pic05 from '../assets/images/karl-andrews.png'
 
 export const PageQuery = graphql`
@@ -18,11 +19,14 @@ export const PageQuery = graphql`
                 mainText
                 title
             }
+            fields {
+                slug
+            }
         }
     }
 `;
 
-export const PageTemplate = ({mainHeading, mainText, title, content, contentComponent}) => {
+export const PageTemplate = ({mainHeading, mainText, title, content, contentComponent, url}) => {
     const PostContent = contentComponent || Content;
 
     return (
@@ -41,8 +45,7 @@ export const PageTemplate = ({mainHeading, mainText, title, content, contentComp
             <div className="inner">
                 <div className="grid-wrapper">
                     <div className="col-8">
-                        <p>Tenacity Interiors has an outstanding reputation for excellence, with installations in the heart of London and New York’s most fashionable districts. Owners Karl Andrews &amp; Trevor Pragg also take pride in being a local company creating beautiful rooms for homes across East Anglia.</p>
-                        <p>As a private client or interior design professional, you will be able to rely on the skills, dedication and care that every project always receives. Tenacity Interiors experts can offer original designs or replicate existing furniture for you, for any room, whatever style you have in mind.</p>
+                        <Share url={url} text="Share This:" />
                     </div>
                     <div className="col-4 complimentary">
                         <p className="col-8">To book your complimentary design consultation today call Karl on <a href="tel:01485 521888"><strong>01485 521888</strong></a></p>
@@ -57,6 +60,7 @@ export const PageTemplate = ({mainHeading, mainText, title, content, contentComp
 
 const Page = ({data}) => {
     const { markdownRemark: post } = data;
+    
     return (
         <Layout
             templateKey={post.frontmatter.templateKey}
@@ -69,6 +73,7 @@ const Page = ({data}) => {
                 contentComponent={HTMLContent}
                 mainHeading={post.frontmatter.mainHeading}
                 mainText={post.frontmatter.mainText}
+                url={post.fields.slug}
             />
         </Layout>
     )
